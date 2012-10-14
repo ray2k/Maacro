56cf2c8a7b94700ccc9b40604bb27296e94f0e2f
Ray O'Neill <ray.oneill@gmail.com>
2012-10-14 13:26:04 -0400

    build setup and minor tweaks

--- build.cmd
+++ build.cmd
@@ -1,9 +1,48 @@
 @call "%VS110COMNTOOLS%\VsDevCmd.bat"
-@del /S /Q .\build\*.*
+@rmdir /S /Q .\build\*.*
 msbuild .\Maacro\Maacro.csproj /t:Clean
-msbuild .\Maacro\Maacro.csproj /t:Build /p:OutDir=..\build\x64\ /p:Configuration=Release 
-/p:Platform=x64
-msbuild .\Maacro\Maacro.csproj /t:Build /p:OutDir=..\build\x86\ /p:Configuration=Release /p:Platform=x86
+msbuild .\Maacro\Maacro.csproj /t:Build /p:SolutionDir=%CD% /p:Platform=x64 /p:OutDir=..\build\x64\ /p:Configuration=Release
+msbuild .\Maacro\Maacro.csproj /t:Build /p:SolutionDir=%CD% /p:Platform=x86 /p:OutDir=..\build\x86\ /p:Configuration=Release
 @del /S /Q .\build\*.pdb
 @del /S /Q .\build\*.xml
+@del /S /Q .\build\*.zip
+@rmdir /S /Q .\build\x86\de\
+@rmdir /S /Q .\build\x86\es\
+@rmdir /S /Q .\build\x86\fr\
+@rmdir /S /Q .\build\x86\it\
+@rmdir /S /Q .\build\x86\ja\
+@rmdir /S /Q .\build\x86\ko\
+@rmdir /S /Q .\build\x86\zh-Hans\
+@rmdir /S /Q .\build\x86\zh-Hant\
+@rmdir /S /Q .\build\x86\de\
+@rmdir /S /Q .\build\x86\es\
+@rmdir /S /Q .\build\x86\fr\
+@rmdir /S /Q .\build\x86\it\
+@rmdir /S /Q .\build\x86\ja\
+@rmdir /S /Q .\build\x86\ko\
+@rmdir /S /Q .\build\x86\zh-Hans\
+@rmdir /S /Q .\build\x86\zh-Hant\
+@rmdir /S /Q .\build\x64\de\
+@rmdir /S /Q .\build\x64\es\
+@rmdir /S /Q .\build\x64\fr\
+@rmdir /S /Q .\build\x64\it\
+@rmdir /S /Q .\build\x64\ja\
+@rmdir /S /Q .\build\x64\ko\
+@rmdir /S /Q .\build\x64\zh-Hans\
+@rmdir /S /Q .\build\x64\zh-Hant\
+@rmdir /S /Q .\build\x64\de\
+@rmdir /S /Q .\build\x64\es\
+@rmdir /S /Q .\build\x64\fr\
+@rmdir /S /Q .\build\x64\it\
+@rmdir /S /Q .\build\x64\ja\
+@rmdir /S /Q .\build\x64\ko\
+@rmdir /S /Q .\build\x64\zh-Hans\
+@rmdir /S /Q .\build\x64\zh-Hant\
+@copy /Y .\build\x86\en\*.dll .\build\x86\*.dll
+@copy /Y .\build\x64\en\*.dll .\build\x64\*.dll
+@rmdir /S /Q .\build\x86\en\
+@rmdir /S /Q .\build\x64\en\
+
+.\Tools\7za a .\Build\Maacro-1.0.0-x86.zip -r .\build\x86\*.*
+.\Tools\7za a .\Build\Maacro-1.0.0-x64.zip -r .\build\x64\*.*
 pause
