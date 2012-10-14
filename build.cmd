79487e64d5a527eb453f7ca852bd835c59fc9f33
Ray O'Neill <ray.oneill@gmail.com>
2012-10-14 03:40:22 -0400

    x86/x64 crap

--- build.cmd
+++ build.cmd
@@ -1,6 +1,9 @@
 @call "%VS110COMNTOOLS%\VsDevCmd.bat"
+@del /S /Q .\build\*.*
 msbuild .\Maacro\Maacro.csproj /t:Clean
-msbuild .\Maacro\Maacro.csproj /t:Build /p:OutDir=..\build /p:Configuration=Release
-@del .\build\*.pdb
-@del .\build\*.xml
+msbuild .\Maacro\Maacro.csproj /t:Build /p:OutDir=..\build\x64\ /p:Configuration=Release 
+/p:Platform=x64
+msbuild .\Maacro\Maacro.csproj /t:Build /p:OutDir=..\build\x86\ /p:Configuration=Release /p:Platform=x86
+@del /S /Q .\build\*.pdb
+@del /S /Q .\build\*.xml
 pause
